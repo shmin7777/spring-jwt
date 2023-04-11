5cee556a49144dfefaf7735e4692bb700e78bdf6
Min Hong <shmin7777@naver.com>
2023-04-11 18:50:12 +0900

    주석 추가

--- src/main/java/com/example/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/config/SecurityConfig.java
@@ -8,6 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 import lombok.RequiredArgsConstructor;
 
+// .httpBasic()
+// header에 authorization에 id, pw를 모든 요청에 넣는다.
+// 암호화가 안되기때문에 위험
+
+// jwt : header에 authorization에 token을 넣는다.
+// id와 pw를 통해서 token을 달고 요청을 하는 방식이 Bearer라는 방식( not Basic )
+
 // @CrossOrigin // 인증이 필요하지 않는 요청만 허용함, 인증이 필요한 요청은 이렇게 하면 안됌
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -22,8 +29,8 @@ public class SecurityConfig {
         return http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(corsFilter) // security filter에 등록해야함
-                .formLogin().disable() // form login을 안하겠다.
-                .httpBasic().disable()
+                .formLogin().disable() // form tag만들어서 login을 안하겠다.
+                .httpBasic().disable() // 기본 로그인 방식이 아닌 bearer라는 방식을 사용할 것이다.
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER')  or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
